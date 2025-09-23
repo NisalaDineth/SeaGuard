4eed05af62acda7a94b0bf66b755253a7565a464
Nisala Dineth <nisala.dineth@mobios.lk>
2025-09-23 11:35:36 +0530

    UI improvements

--- lib/widgets/kpi_card.dart
+++ lib/widgets/kpi_card.dart
@@ -27,8 +27,8 @@ class KpiCard extends StatelessWidget {
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.1),
-            blurRadius: 10,
-            offset: const Offset(0, 4),
+            blurRadius: 4,
+            offset: const Offset(0, 2),
           ),
         ],
       ),
